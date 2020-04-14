fb1c76f3ade372be5f02969cf001b0103ef9a29e
salmo-jr <salmo.jr@gmail.com>
2020-04-14 12:45:33 -0300

    step 1 completed

---
+++ InventorySystem/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    private void Awake()
+    {
+        BuildDatabase();
+    }
+
+    public Item GetItem(int id)
+    {
+        return items.Find(item => item.id == id);
+    }
+
+    public Item GetItem(string title)
+    {
+        return items.Find(item => item.title == title);
+    }
+
+    void BuildDatabase()
+    {
+        items = new List<Item>() {
+            new Item(0, "Diamond Sword", "A sword made with diamond.",
+                new Dictionary<string, int>
+                {
+                    {"Power", 15},
+                    {"Defence", 10}
+                }),
+            new Item(1, "Diamond Ore", "A beautiful diamond",
+                new Dictionary<string, int>
+                {
+                    {"Value", 444}
+                }),
+            new Item(2, "Silver Pick", "A very C# pick",
+                new Dictionary<string, int>
+                {
+                    {"Power", 5},
+                    {"Mining", 333}
+                })
+        };
+    }
+}
