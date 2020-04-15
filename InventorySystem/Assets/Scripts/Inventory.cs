ce1481543d6937b635ddfc95964f851fac238656
salmo-jr <salmo.jr@gmail.com>
2020-04-14 23:19:51 -0300

    step 2 completed

---
+++ InventorySystem/Assets/Scripts/Inventory.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    public List<Item> characterItems = new List<Item>();
+    public ItemDatabase itemDatabase;
+
+    private void Start()
+    {
+        GiveItem("Diamond Sword");
+        GiveItem(1);
+        GiveItem(2);
+        RemoveItem(1);
+    }
+
+    public void GiveItem(int id)
+    {
+        Item itemToAdd = itemDatabase.GetItem(id);
+        GiveItem(itemToAdd);
+    }
+
+    public void GiveItem(string name)
+    {
+        Item itemToAdd = itemDatabase.GetItem(name);
+        GiveItem(itemToAdd);
+    }
+
+    public void GiveItem(Item item)
+    {
+        characterItems.Add(item);
+        Debug.Log("Added item: " + item.title);
+    }
+
+    public Item CheckForItem(int id)
+    {
+        return characterItems.Find(item => item.id == id);
+    }
+
+    public void RemoveItem(int id)
+    {
+        Item item = CheckForItem(id);
+        if (item != null)
+        {
+            characterItems.Remove(item);
+            Debug.Log("Item removed: " + item.title);
+        }
+    }
+}
